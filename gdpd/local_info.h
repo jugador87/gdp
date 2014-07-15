05b3e33841483c344381d92410602ca226fc8dd3
Justin Patel <jplate8@gmail.com>
2014-07-15 13:48:31 -0700

    begin location-independent routing prototype
    still have to:
    - connect to other nodes
    - figure out FIND_OBJ data format
        - figure out ipv6
    - look at existing GCL manipulation commands; massage them and their
      implementations to work nicely over network.
    - stuff I can't think of right now

---
+++ gdpd/local_info.h
@@ -0,0 +1,17 @@
+/*
+ *  LOCAL_INFO.H --- local node-specific info.
+ *
+ *      The info here and in local_info.c should be upon compilation
+ *      to reflect the current node.
+ *      
+ *      XXX this seems pretty jank.
+ */
+
+#ifndef _NODE_INFO_H_
+#define _NODE_INFO_H_
+
+
+extern const gcl_name_t local_node_name;                                
+
+
+#endif //_NODE_INFO_H_
