902961a78229ed0b026d4f2b742805df15b33f0a
Eric Allman <eric.allman@gmail.com>
2014-07-16 10:45:32 -0700

    Fix event loop to avoid 100msec pauses.  There was some old code intended for debugging that should have been pulled out.

--- gdp/gdp_gcl.c
+++ gdp/gdp_gcl.c
@@ -898,12 +898,11 @@ gdp_run_event_loop(void *ctx)
 	    ep_dbg_printf("gdp_event_loop: starting up base loop\n");
 	    event_base_dump_events(evb, ep_dbg_getfile());
 	}
-//#ifdef EVLOOP_NO_EXIT_ON_EMPTY
-//	event_base_loop(evb, EVLOOP_NO_EXIT_ON_EMPTY);
-//#else
-//	event_base_loop(evb, 0);
-//#endif
-	event_base_loop(evb, EVLOOP_ONCE);
+#ifdef EVLOOP_NO_EXIT_ON_EMPTY
+	event_base_loop(evb, EVLOOP_NO_EXIT_ON_EMPTY);
+#else
+	event_base_loop(evb, 0);
+#endif
 	// shouldn't happen (?)
 	ep_dbg_cprintf(Dbg, 1, "gdp_event_loop: event_base_loop returned\n");
 	if (evdelay > 0)
