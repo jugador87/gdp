75caab76896ea1d8ebac5b93405ab6eb851cedfa
Eric Allman <eric.allman@gmail.com>
2014-08-01 15:55:11 -0700

    Add ep_time.[ch] --- it's not clear why they didn't get added.

---
+++ ep/ep_time.h
@@ -0,0 +1,21 @@
+/* vim: set ai sw=8 sts=8 ts=8 :*/
+
+#ifndef _EP_TIME_H_
+#define _EP_TIME_H_
+
+#include <time.h>
+#include <sys/time.h>
+#include <stdint.h>
+
+typedef struct
+{
+	int64_t		tv_sec;
+	uint32_t	tv_nsec;
+} EP_TIME_SPEC;
+
+extern EP_STAT	ep_time_now(EP_TIME_SPEC *tv);
+extern EP_STAT	ep_time_nanosleep(int64_t);
+
+#define EP_TIME_NOTIME		(INT64_MIN)
+
+#endif //_EP_TIME_H_
