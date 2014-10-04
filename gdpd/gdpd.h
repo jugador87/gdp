6b3dbef424ce1bf643eebba52673fd6617b878e3
Eric Allman <eric@cs.berkeley.edu>
2014-10-04 17:35:05 +0200

    Break out gdpd GCL handling into separate file.

--- gdpd/gdpd.h
+++ gdpd/gdpd.h
@@ -45,4 +45,32 @@ struct gdp_gcl_xtra
 	off_t				data_offset;	// offset for start of data
 };
 
+
+/*
+**  Definitions for the gdpd-specific GCL handling
+*/
+
+extern EP_STAT	gcl_alloc(				// allocate a new GCL
+					gcl_name_t gcl_name,
+					gdp_iomode_t iomode,
+					gdp_gcl_t **pgclh);
+
+extern EP_STAT	gcl_open(				// open an existing physical GCL
+					gcl_name_t gcl_name,
+					gdp_iomode_t iomode,
+					gdp_gcl_t **pgclh);
+
+extern void		gcl_close(				// close an open GCL
+					gdp_gcl_t *gclh);
+
+extern void		gcl_touch(				// make a GCL recently used
+					gdp_gcl_t *gclh);
+
+extern void		gcl_showusage(			// show GCL LRU list
+					FILE *fp);
+
+extern EP_STAT	get_open_handle(		// get open handle (pref from cache)
+					gdp_req_t *req,
+					gdp_iomode_t iomode);
+
 #endif //_GDPD_H_
