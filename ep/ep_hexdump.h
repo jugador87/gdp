fbeef333b9f87e5522ba0f67dd0a7bf3295313ed
Eric Allman <eric@cs.berkeley.edu>
2014-11-06 08:56:29 -0800

    Include ability to print ASCII version.

--- ep/ep_hexdump.h
+++ ep/ep_hexdump.h
@@ -1,3 +1,9 @@
 /* vim: set ai sw=8 sts=8 ts=8 :*/
 
-void	ep_hexdump(void *bufp, size_t buflen, FILE *fp, int format);
+/*
+**  The format parameter tweaks the output.
+*/
+
+#define EP_HEXDUMP_ASCII	0x0001	// show ASCII equivalent
+
+void	ep_hexdump(const void *bufp, size_t buflen, FILE *fp, int format);
