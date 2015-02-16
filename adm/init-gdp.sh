874dd6e450ca92dfe09ff0feeb24d77071459f40
Eric Allman <eric@cs.berkeley.edu>
2015-02-16 11:15:44 -0800

    Include initialization script (specific to machines running daemons at Berkeley)

---
+++ adm/init-gdp.sh
@@ -0,0 +1,62 @@
+#!/bin/sh
+
+#
+#  Initialize server hosts
+#	This is specialized for Berkeley
+#
+
+if [ `whoami` != 'gdp' ]
+then
+	echo "This script must run as user gdp"
+	if ! grep -q '^gdp:' /etc/passwd
+	then
+		echo "I will create the user for you"
+		sudo adduser --system --group gdp
+	fi
+	echo "Please try again"
+	exit 1
+fi
+
+cd ~gdp
+home=`pwd`
+umask 0002
+
+[ -d bin ] || mkdir bin
+[ -d log ] || mkdir log
+[ -d db ] || mkdir db
+[ -d .ep_adm_params ] || mkdir .ep_adm_params
+
+hostname=`hostname`
+if echo $hostname | grep -q '^gdp-0'
+then
+	routers="127.0.0.1"
+	for i in 01 02 03
+	do
+		if [ $hostname != "gdp-$i" ]
+		then
+			routers="$routers; gdp-$i.eecs.berkeley.edu"
+		fi
+	done
+else
+	routers="gdp-01.eecs.berkeley.edu; gdp-02.eecs.berkeley.edu; gdp-03.eecs.berkeley.edu"
+fi
+
+if [ ! -f .ep_adm_params/gdp ]
+then (
+	echo "swarm.gdp.routers=$routers"
+	) > .ep_adm_params/gdp
+else
+	echo "Warning: .ep_adm_params/gdp already exists; check consistency" 2>&1
+fi
+
+if [ ! -f .ep_adm_params/gdplogd ]
+then (
+	echo "swarm.gdplogd.gdpname=edu.berkeley.eecs.$hostname.gdplogd"
+	echo "swarm.gdplogd.gcl.dir=$home/db/gcl"
+	) > .ep_adm_params/gdplogd
+else
+	echo "Warning: .ep_adm_params/gdplogd already exists; check consistency" 2>&1
+fi
+
+umask 0007
+[ -d db/gcl ] || mkdir db/gcl
