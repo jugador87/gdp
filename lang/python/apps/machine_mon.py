a55719a665bee2ca3a05524b8ca9401251633020
Nitesh Mor <mor.nitesh@gmail.com>
2015-02-17 14:07:31 -0800

    Another argument for machinemon

--- lang/python/apps/machine_mon.py
+++ lang/python/apps/machine_mon.py
@@ -16,7 +16,7 @@ import json
 
 SLEEP_INTERVAL = 60
 
-def main(name_str):
+def main(name_str, hostname):
 
     # Create a GDP_NAME object from a python string provided as argument
     gcl_name = gdp.GDP_NAME(name_str)
@@ -28,6 +28,7 @@ def main(name_str):
 
         # Collect information that we are going to log
         d = {}
+        d['host'] = hostname
         l = os.getloadavg()
         d['load1'], d['load5'], d['load15'] = l[0], l[1], l[2]
         d['freeram'] = psutil.phymem_usage().free
@@ -44,10 +45,10 @@ def main(name_str):
 
 if __name__ == "__main__":
 
-    if len(sys.argv) < 2:
-        print "Usage: %s <gcl_name>" % sys.argv[0]
+    if len(sys.argv) < 3:
+        print "Usage: %s <hostname> <gcl_name>" % sys.argv[0]
         sys.exit(1)
 
     # Change this to point to a gdp_router
     gdp.gdp_init()
-    main(sys.argv[1])
+    main(sys.argv[2], sys.argv[1])
