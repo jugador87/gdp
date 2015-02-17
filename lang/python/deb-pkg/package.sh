67fe2c7cad289c06a44353208e7b64320312bb0b
Nitesh Mor <mor.nitesh@gmail.com>
2015-02-17 12:09:26 -0800

    minor changes

--- lang/python/deb-pkg/package.sh
+++ lang/python/deb-pkg/package.sh
@@ -10,13 +10,13 @@ curdir=`dirname $0`
 topdir="$curdir/../"
 tmpdir="/tmp/python-gdp_0.1-1"
 pydir="/usr/lib/python2.7/dist-packages/"
-sharedir="/usr/share/python-gdp/"
+sharedir="/usr/share/doc/gdp/python-gdp"
 
 
 rm -rf $tmpdir
 mkdir $tmpdir
 mkdir -p $tmpdir/$pydir/gdp
-mkdir -p $tmpdir/$sharedir/examples
+mkdir -p $tmpdir/$sharedir/
 
 
 cp -a $topdir/gdp/*.py $tmpdir/$pydir/gdp/.
@@ -24,8 +24,8 @@ cp -a $topdir/gdp/*.py $tmpdir/$pydir/gdp/.
 sed -i "s/\"..\", \"..\", \"..\", \"libs\"/\"\/\", \"usr\", \"lib\"/g" $tmpdir/$pydir/gdp/MISC.py
 
 # documentation and examples
-cp $topdir/README $tmpdir/$sharedir/.
-cp -a $topdir/apps/*.py $tmpdir/$sharedir/examples
+cp $topdir/README $tmpdir/$sharedir/
+cp -a $topdir/apps/*.py $tmpdir/$sharedir/
 sed -i "s/sys.path.append/# sys.path.append/g" $tmpdir/$sharedir/examples/*.py
 
 mkdir $tmpdir/DEBIAN
@@ -33,8 +33,7 @@ echo "Package: python-gdp
 Version: 0.1-1
 Priority: optional
 Architecture: all
-Depends: python (<< 2.8), python (>= 2.7~), libgdp (>=0.1-1)
-Recommends: python-psutil (>= 1.2~)
+Depends: python (<< 2.8), python (>= 2.7~), libgdp (>=0.1-1), libevent-dev (>2.0~), python-psutil (>= 1.2~)
 Maintainer: Nitesh Mor <mor@eecs.berkeley.edu>
 Description: A python interface for libGDP
  Some sample programs are located at $sharedir/examples
