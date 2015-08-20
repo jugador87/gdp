f43e276d62ee570db0025bbd7a928f19b643e40b
Eric Allman <eric@cs.berkeley.edu>
2015-08-20 15:58:47 -0700

    Allow logs to opened for both read and append (GDP_MODE_RA).

--- gdp/gdp.h
+++ gdp/gdp.h
@@ -73,6 +73,7 @@ typedef enum
 	GDP_MODE_ANY =		0,				// no mode specified
 	GDP_MODE_RO =		0x0001,			// readable
 	GDP_MODE_AO =		0x0002,			// appendable
+	GDP_MODE_RA =		0x0003,			// read + append
 } gdp_iomode_t;
 
 
