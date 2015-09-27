6ce0510350f90cdff5a685cff83fb5128811cae2
Eric Allman <eric@cs.berkeley.edu>
2015-09-27 16:32:31 -0700

    Add commentary about the possible 64-bit NUXI problem on 32-bit hardware.

--- ep/ep_net.c
+++ ep/ep_net.c
@@ -7,6 +7,24 @@
 #include "ep.h"
 #include "ep_net.h"
 
+/*
+**  Network support routines.
+**
+**	Right now these mostly have to do with endianness.
+**
+**	I assume that 64 bits are transmitted in pure big-endian format;
+**	for example 0x000102030405060708090A0B0C0D0E0F is transmitted as
+**	00 01 02 03 04 05 06 07 08 09 0A 0B 0C 0D 0E 0F.  On 32-bit
+**	machines we might have a NUXI problem that would transmit as
+**	08 09 0A 0B 0C 0D 0E 0F 00 01 02 03 04 05 06 07.
+**
+**	XXX THIS NEEDS MORE RESEARCH
+*/
+
+#ifndef EP_HWCF_64_BIT_NUXI_PROBLEM
+# define EP_HWCF_64_BIT_NUXI_PROBLEM	0
+#endif
+
 uint64_t
 _ep_net_swap64(uint64_t v)
 {
@@ -17,8 +35,14 @@ _ep_net_swap64(uint64_t v)
 	} a, b;
 
 	a.i64 = v;
+
+#if EP_HWCF_64_BIT_NUXI_PROBLEM
+	b.i32[0] = ep_net_hton32(a.i32[0]);
+	b.i32[1] = ep_net_hton32(a.i32[1]);
+#else
 	b.i32[0] = ep_net_hton32(a.i32[1]);
 	b.i32[1] = ep_net_hton32(a.i32[0]);
+#endif
 	return b.i64;
 }
 
