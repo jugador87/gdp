ca3929c7a8102fc8279334602c46ec6b8375e5fa
Eric Allman <eric@cs.berkeley.edu>
2015-10-05 11:17:36 -0700

    Re-arrange the file to cluster function declarations with the corresponding data structures.  No semantic changes.

--- gdp/gdp_priv.h
+++ gdp/gdp_priv.h
@@ -15,6 +15,7 @@
 #include <stdio.h>
 
 typedef struct gdp_chan		gdp_chan_t;
+typedef struct gdp_req		gdp_req_t;
 
 extern pthread_t	_GdpIoEventLoopThread;
 gdp_chan_t			*_GdpChannel;		// our primary app-level protocol port
@@ -68,6 +69,13 @@ struct gdp_chan
 #define GDP_CHAN_ERROR			3		// channel has had error
 #define GDP_CHAN_CLOSING		4		// channel is closing
 
+EP_STAT			_gdp_chan_open(				// open channel to routing layer
+						const char *gdpd_addr,
+						void (*process)(gdp_pdu_t *, gdp_chan_t *),
+						gdp_chan_t **pchan);
+void			_gdp_chan_close(			// close channel
+						gdp_chan_t **pchan);
+
 
 /*
 **	 Datums
@@ -89,6 +97,12 @@ struct gdp_datum
 	short				siglen;			// signature length
 	bool				inuse:1;		// the datum is in use (for debugging)
 };
+
+// dump data record (for debugging)
+extern void		_gdp_datum_dump(
+					const gdp_datum_t *datum,	// message to print
+					FILE *fp);					// file to print it to
+
 
 
 /*
@@ -128,150 +142,6 @@ struct gdp_gcl
 #define GCLF_ISLOCKED		0x0004		// GclCacheMutex already locked
 #define GCLF_INUSE			0x0008		// handle is allocated
 #define GCLF_DEFER_FREE		0x0010		// defer actual free until reclaim
-
-
-/*
-**  A Work Request (and associated Response)
-**
-**		A GDP request is packaged up in one of these things and
-**		submitted.  Responses are returned in the same structure.
-**
-**		There is one PDU header here that is used both for
-**		sending and receiving.  The associated gdp_buf_t does not
-**		have a write callback, so it can be used without having
-**		any side effects.  The PDU header has much of the
-**		information, including the command/response code, the rid,
-**		the record number, the timestamp, and the data buffer.
-**
-**		There can be mulitple requests active on a single GCL at
-**		any time, but they should have unique rids.  Rids can be
-**		reused if desired once an operation is complete.  Note:
-**		some operations (e.g., subscriptions) can return multiple
-**		results, but they will have the same rid.
-**
-**		Requests are potentially linked on lists.  Every request
-**		that is active on a channel is linked to that channel
-**		(with the GDP_REQ_ON_CHAN_LIST flag set); this is so that
-**		requests can be cleaned up if the channel goes away.  At
-**		this point we try to recover the channel, so this should
-**		be rare, but that list is also used to find requests that
-**		need to be timed out.
-**
-**		For active requests --- that is, requests that are either
-**		waiting for a response (in _gdp_invoke) or represent
-**		potential points for subscriptions --- are also linked to
-**		the corresponding GCL, and will have the GDP_REQ_ON_GCL_LIST
-**		flag set.  Subscription listeners also have the
-**		GDP_REQ_CLT_SUBSCR flag set.  GDP_REQ_SRV_SUBSCR is used
-**		by gdplogd to find the other end of the subscription, i.e,
-**		subscription data producers.
-**
-**		In both the case of applications and gdplogd, requests may
-**		get passed between threads.  To prevent someone from finding
-**		a request on one of these lists and using it at the same time
-**		someone else has it in use, you would like to lock the data
-**		structure while it is active.  But you can't pass a mutex
-**		between threads.  This is a particular problem if subscription
-**		or multiread data comes in faster than it can be processed;
-**		since the I/O thread is separate from the processing thread
-**		things can clobber each other.
-**
-**		We solve this by assigning a state to each request:
-**
-**		FREE means that this request is on the free list.  It
-**			should never appear in any other context.
-**		ACTIVE means that there is currently an operation taking
-**			place on the request, and no one other than the owner
-**			should use it.  If you need it, you can wait on the
-**			condition variable.
-**		WAITING means that the request has been sent from a client
-**			to a server but hasn't gotten the response yet.  It
-**			shouldn't be possible for a WAITING request to also
-**			have an active subscription, but it will be in the GCL
-**			list.
-**		IDLE means that the request is not free, but there is no
-**			operation in process on it.  This will generally be
-**			because it is a subscription that does not have any
-**			currently active data.
-**
-**		If you want to deliver data to a subscription, you have to
-**		first make sure the req is in IDLE state, turn it to ACTIVE
-**		state, and then process it.  If it is not in IDLE state you
-**		sleep on the condition variable and try again.
-**
-**		Passing a request to another thread is basically the same.
-**		The invariant is that any req being passed between threads
-**		should always be ACTIVE.
-*/
-
-typedef struct gdp_req
-{
-	EP_THR_MUTEX		mutex;		// lock on this data structure
-	EP_THR_COND			cond;		// pthread wakeup condition variable
-	LIST_ENTRY(gdp_req)	gcllist;	// linked list for cache management
-	LIST_ENTRY(gdp_req)	chanlist;	// reqs associated with a given channel
-	gdp_gcl_t			*gcl;		// the corresponding GCL handle
-	gdp_pdu_t			*pdu;		// PDU buffer
-	gdp_chan_t			*chan;		// the network channel for this req
-	EP_STAT				stat;		// status code from last operation
-	int32_t				numrecs;	// remaining number of records to return
-	uint16_t			state;		// see below
-	uint32_t			flags;		// see below
-	EP_TIME_SPEC		sub_ts;		// timestamp of last subscription activity
-	void				(*postproc)(struct gdp_req *);
-									// do post processing after ack sent
-	gdp_event_cbfunc_t	sub_cb;		// callback function (subscribe & async I/O)
-	void				*udata;		// user-supplied opaque data to cb
-	EP_CRYPTO_MD		*md;		// message digest context
-} gdp_req_t;
-
-// states
-#define GDP_REQ_FREE			0			// request is free
-#define GDP_REQ_ACTIVE			1			// currently being processed
-#define GDP_REQ_WAITING			2			// waiting on cond variable
-#define GDP_REQ_IDLE			3			// subscription waiting for data
-
-// flags
-#define GDP_REQ_ASYNCIO			0x00000001	// async I/O operation
-#define GDP_REQ_DONE			0x00000002	// operation complete
-#define GDP_REQ_CLT_SUBSCR		0x00000004	// client-side subscription
-#define GDP_REQ_SRV_SUBSCR		0x00000008	// server-side subscription
-#define GDP_REQ_PERSIST			0x00000010	// request persists after response
-#define GDP_REQ_SUBUPGRADE		0x00000020	// can upgrade to subscription
-#define GDP_REQ_ALLOC_RID		0x00000040	// force allocation of new rid
-#define GDP_REQ_ON_GCL_LIST		0x00000080	// this is on a GCL list
-#define GDP_REQ_ON_CHAN_LIST	0x00000100	// this is on a channel list
-#define GDP_REQ_CORE			0x00000200	// internal to the core code
-
-extern void			_gdp_req_lock(gdp_req_t *);
-extern void			_gdp_req_unlock(gdp_req_t *);
-extern gdp_req_t	*_gdp_req_find(gdp_gcl_t *gcl, gdp_rid_t rid);
-extern gdp_rid_t	_gdp_rid_new(gdp_gcl_t *gcl, gdp_chan_t *chan);
-extern EP_STAT		_gdp_req_send(gdp_req_t *req);
-extern EP_STAT		_gdp_req_unsend(gdp_req_t *req);
-
-
-/*
-**  Structure used for registering command functions
-**
-**		The names are already known to the GDP library, so this is just
-**		to bind functions that implement the individual commands.
-*/
-
-typedef EP_STAT	cmdfunc_t(			// per command dispatch entry
-					gdp_req_t *req);	// the request to be processed
-
-struct cmdfuncs
-{
-	int			cmd;				// command number
-	cmdfunc_t	*func;				// pointer to implementing function
-};
-
-void			_gdp_register_cmdfuncs(
-						struct cmdfuncs *);
-
-const char		*_gdp_proto_cmd_name(		// return printable cmd name
-						uint8_t cmd);
 
 /*
 **  GCL cache.
@@ -398,23 +268,121 @@ EP_STAT			_gdp_gcl_getmetadata(		// retrieve metadata
 						gdp_chan_t *chan,
 						uint32_t reqflags);
 
-/*
-**  Initialization.
-*/
-
-EP_STAT			_gdp_start_event_loop_thread(
-						pthread_t *thr,
-						struct event_base *evb,
-						const char *where);
-
-void			_gdp_newname(gdp_name_t gname,
-						gdp_gclmd_t *gmd);
 
 /*
-**  Request handling.
+**  A Work Request (and associated Response)
 **
-**  Implemented in gdp/gdp_req.c.
+**		A GDP request is packaged up in one of these things and
+**		submitted.  Responses are returned in the same structure.
+**
+**		There is one PDU header here that is used both for
+**		sending and receiving.  The associated gdp_buf_t does not
+**		have a write callback, so it can be used without having
+**		any side effects.  The PDU header has much of the
+**		information, including the command/response code, the rid,
+**		the record number, the timestamp, and the data buffer.
+**
+**		There can be mulitple requests active on a single GCL at
+**		any time, but they should have unique rids.  Rids can be
+**		reused if desired once an operation is complete.  Note:
+**		some operations (e.g., subscriptions) can return multiple
+**		results, but they will have the same rid.
+**
+**		Requests are potentially linked on lists.  Every request
+**		that is active on a channel is linked to that channel
+**		(with the GDP_REQ_ON_CHAN_LIST flag set); this is so that
+**		requests can be cleaned up if the channel goes away.  At
+**		this point we try to recover the channel, so this should
+**		be rare, but that list is also used to find requests that
+**		need to be timed out.
+**
+**		For active requests --- that is, requests that are either
+**		waiting for a response (in _gdp_invoke) or represent
+**		potential points for subscriptions --- are also linked to
+**		the corresponding GCL, and will have the GDP_REQ_ON_GCL_LIST
+**		flag set.  Subscription listeners also have the
+**		GDP_REQ_CLT_SUBSCR flag set.  GDP_REQ_SRV_SUBSCR is used
+**		by gdplogd to find the other end of the subscription, i.e,
+**		subscription data producers.
+**
+**		In both the case of applications and gdplogd, requests may
+**		get passed between threads.  To prevent someone from finding
+**		a request on one of these lists and using it at the same time
+**		someone else has it in use, you would like to lock the data
+**		structure while it is active.  But you can't pass a mutex
+**		between threads.  This is a particular problem if subscription
+**		or multiread data comes in faster than it can be processed;
+**		since the I/O thread is separate from the processing thread
+**		things can clobber each other.
+**
+**		We solve this by assigning a state to each request:
+**
+**		FREE means that this request is on the free list.  It
+**			should never appear in any other context.
+**		ACTIVE means that there is currently an operation taking
+**			place on the request, and no one other than the owner
+**			should use it.  If you need it, you can wait on the
+**			condition variable.
+**		WAITING means that the request has been sent from a client
+**			to a server but hasn't gotten the response yet.  It
+**			shouldn't be possible for a WAITING request to also
+**			have an active subscription, but it will be in the GCL
+**			list.
+**		IDLE means that the request is not free, but there is no
+**			operation in process on it.  This will generally be
+**			because it is a subscription that does not have any
+**			currently active data.
+**
+**		If you want to deliver data to a subscription, you have to
+**		first make sure the req is in IDLE state, turn it to ACTIVE
+**		state, and then process it.  If it is not in IDLE state you
+**		sleep on the condition variable and try again.
+**
+**		Passing a request to another thread is basically the same.
+**		The invariant is that any req being passed between threads
+**		should always be ACTIVE.
+**
+**		Implemented in gdp_req.c.
 */
+
+struct gdp_req
+{
+	EP_THR_MUTEX		mutex;		// lock on this data structure
+	EP_THR_COND			cond;		// pthread wakeup condition variable
+	LIST_ENTRY(gdp_req)	gcllist;	// linked list for cache management
+	LIST_ENTRY(gdp_req)	chanlist;	// reqs associated with a given channel
+	gdp_gcl_t			*gcl;		// the corresponding GCL handle
+	gdp_pdu_t			*pdu;		// PDU buffer
+	gdp_chan_t			*chan;		// the network channel for this req
+	EP_STAT				stat;		// status code from last operation
+	int32_t				numrecs;	// remaining number of records to return
+	uint16_t			state;		// see below
+	uint32_t			flags;		// see below
+	EP_TIME_SPEC		sub_ts;		// timestamp of last subscription activity
+	void				(*postproc)(struct gdp_req *);
+									// do post processing after ack sent
+	gdp_event_cbfunc_t	sub_cb;		// callback function (subscribe & async I/O)
+	void				*udata;		// user-supplied opaque data to cb
+	EP_CRYPTO_MD		*md;		// message digest context
+};
+
+// states
+#define GDP_REQ_FREE			0			// request is free
+#define GDP_REQ_ACTIVE			1			// currently being processed
+#define GDP_REQ_WAITING			2			// waiting on cond variable
+#define GDP_REQ_IDLE			3			// subscription waiting for data
+
+// flags
+#define GDP_REQ_ASYNCIO			0x00000001	// async I/O operation
+#define GDP_REQ_DONE			0x00000002	// operation complete
+#define GDP_REQ_CLT_SUBSCR		0x00000004	// client-side subscription
+#define GDP_REQ_SRV_SUBSCR		0x00000008	// server-side subscription
+#define GDP_REQ_PERSIST			0x00000010	// request persists after response
+#define GDP_REQ_SUBUPGRADE		0x00000020	// can upgrade to subscription
+#define GDP_REQ_ALLOC_RID		0x00000040	// force allocation of new rid
+#define GDP_REQ_ON_GCL_LIST		0x00000080	// this is on a GCL list
+#define GDP_REQ_ON_CHAN_LIST	0x00000100	// this is on a channel list
+#define GDP_REQ_CORE			0x00000200	// internal to the core code
 
 EP_STAT			_gdp_req_new(				// create new request
 						int cmd,
@@ -425,6 +393,24 @@ EP_STAT			_gdp_req_new(				// create new request
 						gdp_req_t **reqp);
 
 void			_gdp_req_free(				// free old request
+						gdp_req_t *req);
+
+void			_gdp_req_lock(				// lock a request mutex
+						gdp_req_t *);
+
+void			_gdp_req_unlock(			// unlock a request mutex
+						gdp_req_t *);
+
+gdp_req_t		*_gdp_req_find(				// find a request in a GCL
+						gdp_gcl_t *gcl, gdp_rid_t rid);
+
+gdp_rid_t		_gdp_rid_new(				// create new request id
+						gdp_gcl_t *gcl, gdp_chan_t *chan);
+
+EP_STAT			_gdp_req_send(				// send a request to log daemon
+						gdp_req_t *req);
+
+EP_STAT			_gdp_req_unsend(			// pull failed request off GCL list 
 						gdp_req_t *req);
 
 EP_STAT			_gdp_req_dispatch(			// do local req processing
@@ -445,6 +431,41 @@ void			_gdp_req_dump(				// print (debug) request
 
 void			_gdp_chan_drain_input(		// drain all input from channel
 						gdp_chan_t *chan);
+
+
+/*
+**  Structure used for registering command functions
+**
+**		The names are already known to the GDP library, so this is just
+**		to bind functions that implement the individual commands.
+*/
+
+typedef EP_STAT	cmdfunc_t(			// per command dispatch entry
+					gdp_req_t *req);	// the request to be processed
+
+struct cmdfuncs
+{
+	int			cmd;				// command number
+	cmdfunc_t	*func;				// pointer to implementing function
+};
+
+void			_gdp_register_cmdfuncs(
+						struct cmdfuncs *);
+
+const char		*_gdp_proto_cmd_name(		// return printable cmd name
+						uint8_t cmd);
+
+/*
+**  Initialization.
+*/
+
+EP_STAT			_gdp_start_event_loop_thread(
+						pthread_t *thr,
+						struct event_base *evb,
+						const char *where);
+
+void			_gdp_newname(gdp_name_t gname,
+						gdp_gclmd_t *gmd);
 
 
 /*
@@ -473,17 +494,6 @@ void			_gdp_subscr_poke(			// test subscriptions still alive
 						gdp_chan_t *chan);
 
 /*
-**  Communications.
-*/
-
-EP_STAT			_gdp_chan_open(				// open channel to routing layer
-						const char *gdpd_addr,
-						void (*process)(gdp_pdu_t *, gdp_chan_t *),
-						gdp_chan_t **pchan);
-void			_gdp_chan_close(			// close channel
-						gdp_chan_t **pchan);
-
-/*
 **  Cryptography support
 */
 
@@ -505,15 +515,6 @@ struct event_loop_info
 };
 
 EP_STAT			_gdp_evloop_init(void);		// start event loop
-
-/*
-**  Miscellaneous
-*/
-
-// dump data record (for debugging)
-extern void		_gdp_datum_dump(
-					const gdp_datum_t *datum,	// message to print
-					FILE *fp);					// file to print it to
 
 
 #endif // _GDP_PRIV_H_
