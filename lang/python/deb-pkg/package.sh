d7cb38795eba026b3a7bce9048cd7657b249e9a6
Nitesh Mor <mor.nitesh@gmail.com>
2016-01-11 13:08:32 -0800

    Added licenses for pyhton

--- lang/python/deb-pkg/package.sh
+++ lang/python/deb-pkg/package.sh
@@ -1,5 +1,32 @@
 #!/bin/sh
 
+# ----- BEGIN LICENSE BLOCK ----- 
+#	GDP: Global Data Plane
+#	From the Ubiquitous Swarm Lab, 490 Cory Hall, U.C. Berkeley.
+#
+#	Copyright (c) 2015, Regents of the University of California.
+#	All rights reserved.
+#
+#	Permission is hereby granted, without written agreement and without
+#	license or royalty fees, to use, copy, modify, and distribute this
+#	software and its documentation for any purpose, provided that the above
+#	copyright notice and the following two paragraphs appear in all copies
+#	of this software.
+#
+#	IN NO EVENT SHALL REGENTS BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT,
+#	SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST
+#	PROFITS, ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION,
+#	EVEN IF REGENTS HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+#	REGENTS SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT
+#	LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+#	FOR A PARTICULAR PURPOSE. THE SOFTWARE AND ACCOMPANYING DOCUMENTATION,
+#	IF ANY, PROVIDED HEREUNDER IS PROVIDED "AS IS". REGENTS HAS NO
+#	OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS,
+#	OR MODIFICATIONS.
+# ----- END LICENSE BLOCK ----- 
+
+
 # Create a debian package. Some reasons this should be a deb-package:
 # - Certain dependencies (python/ctypes/psutil/...)
 # - Ideal if installed in default python path
@@ -27,12 +54,15 @@ mkdir -p $tmpdir/$sharedir/
 
 
 cp -a $topdir/gdp/*.py $tmpdir/$pydir/gdp/.
+cp -a $topdir/gdp/LICENSE $tmpdir/$pydir/gdp/.
 # fix library locations
 sed -i "s/\"..\", \"..\", \"..\", \"libs\"/\"\/\", \"usr\", \"lib\"/g" $tmpdir/$pydir/gdp/MISC.py
 
 # documentation and examples
 cp $topdir/README $tmpdir/$sharedir/
+cp $topdir/LICENSE $tmpdir/$sharedir/
 cp -a $topdir/apps/*.py $tmpdir/$sharedir/
+cp -a $topdir/apps/LICENSE $tmpdir/$sharedir/
 sed -i "s/sys.path.append/# sys.path.append/g" $tmpdir/$sharedir/*.py
 
 mkdir $tmpdir/DEBIAN
