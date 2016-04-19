a29554b7f91d8ae592298aab0c134b68532b338d
Eric Allman <eric@cs.berkeley.edu>
2016-04-19 11:34:39 -0700

    Make mqtt-gdp startup script more generic.

--- uhb/start-immersion-room.sh
+++ uhb/start-immersion-room.sh
@@ -1,20 +1,56 @@
 #!/bin/sh
 
+cd `dirname $0`/..
+root=`pwd`
+. $root/adm/common-support.sh
+
+# set defaults
 groot="edu.berkeley.eecs.swarmlab.immersion-room.device"
 mqtt_host="uhkbbb001.eecs.berkeley.edu"
+
+args=`getopt M:r: $*`
+if [ $? != 0 ]
+then
+	fatal "Usage: $0 [ -M mqtt-broker ] [ -r logname-root ]"
+fi
+
+set -- $args
+for i
+do
+	case "$i"
+	in
+		-M)
+			mqtt_host="$2"
+			shift; shift;;
+
+		-r)
+			groot="$2"
+			shift; shift;;
+		--)
+			shift; break;;
+	esac
+done
+
+info "Using log names $groot/*"
+info "Using MQTT server at $mqtt_host"
+
 args="-s -M $mqtt_host -d"
 gw_prog="uhb/mqtt-gdp-gateway"
 devices=`mosquitto_sub -h $mqtt_host -C 1 -t gateway-topics | \
 	sed -e 's/^..//' -e 's/..$//' -e 's/", *"/ /g' -e 's/device\/[a-zA-Z]*\///g'`
-echo Devices: $devices
+if [ -z "$devices" ]
+then
+	fatal "Cannot locate any devices to log"
+fi
+info Devices: $devices
 for i in $devices
 do
 	if apps/gcl-create -q -e none $groot.$i
 	then
-		echo "Created log $groot.$i"
+		info "Created log $groot.$i"
 	fi
 	args="$args device/+/$i $groot.$i"
 done
 
-echo "running $gw_prog $args"
+info "running $gw_prog $args"
 $gw_prog $args
