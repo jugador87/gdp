742e87baa8186e59890d7b8ddae1fbf932b71d4a
Nitesh Mor <mor.nitesh@gmail.com>
2016-06-05 14:43:17 -0700

    Visualization: server code cleanup

--- uhb/visualization/server.py
+++ uhb/visualization/server.py
@@ -7,14 +7,13 @@ A visualization tool for time-series data stored in GDP logs
 from twisted.internet import reactor
 from twisted.web.resource import Resource
 from twisted.web.server import Site
-from twisted.web.resource import NoResource
 
 import gdp
-import urlparse
 import argparse
 import json
 import gviz_api
 from datetime import datetime
+import time
 
 
 class DataResource(Resource):
@@ -27,89 +26,93 @@ class DataResource(Resource):
     def __handleQuery(self, request):
 
         args = request.args
-        response = ""
 
-        try:
+        # get query parameters
+        logname = args['logname'][0]
+        startRec = int(args['startRec'][0])
+        endRec = int(args['endRec'][0])
+        step = int(args['step'][0])
+        key = args['key'][0]
 
-            # get query parameters
-            logname = args['logname'][0]
-            startRec = int(args['startRec'][0])
-            endRec = int(args['endRec'][0])
-            step = int(args['step'][0])
-            key = args['key'][0]
+        # reqId processing
+        reqId = 0
+        tqx = args['tqx'][0].split(';')
+        for t in tqx:
+            _t = t.split(':')
+            if _t[0] == "reqId":
+                reqId = _t[1]
+                break
 
-            # reqId processing
-            reqId = 0
-            tqx = args['tqx'][0].split(';')
-            for t in tqx:
-                _t = t.split(':')
-                if _t[0] == "reqId":
-                    reqId = _t[1]
-                    break
+        # open GDP log
+        lh = gdp.GDP_GCL(gdp.GDP_NAME(logname), gdp.GDP_MODE_RO)
 
-            lh = gdp.GDP_GCL(gdp.GDP_NAME(logname), gdp.GDP_MODE_RO)
+        mostrecent = lh.read(-1)['recno']
 
-            mostrecent = lh.read(-1)['recno']
+        # create data
+        reclist = range(startRec, endRec, step)
+        data = []
+        for t in reclist:
 
-            # create data
-            reclist = range(startRec, endRec, step)
-            data = []
-            for t in reclist:
+            # make sure we are in the right range
+            if (t>=0 and (t<1 or t>mostrecent)) or \
+                    (t<0 and (t*(-1)>=mostrecent)):
+                break
 
-                # make sure we are in the right range
-                if (t>=0 and (t<1 or t>mostrecent)) or \
-                        (t<0 and (t*(-1)>=mostrecent)):
-                    break
+            # go read the data
+            datum = lh.read(t)
+            _time = datetime.fromtimestamp(datum['ts']['tv_sec'] + \
+                                (datum['ts']['tv_nsec']*1.0/10**9))
+            _raw_data = json.loads(datum['data'])[key]
+            if _raw_data == "true":
+                _data = 1.0
+            elif _raw_data == "false":
+                _data = 0.0
+            else:
+                _data = float(_raw_data)
+            data.append((_time,_data))
 
-                # go read the data
-                datum = lh.read(t)
-                _time = datetime.fromtimestamp(datum['ts']['tv_sec'] + \
-                                    (datum['ts']['tv_nsec']*1.0/10**9))
-                _raw_data = json.loads(datum['data'])[key]
-                if _raw_data == "true":
-                    _data = 1.0
-                elif _raw_data == "false":
-                    _data = 0.0
-                else:
-                    _data = float(_raw_data)
-                data.append((_time,_data))
+        desc = [('time', 'datetime'), ('key', 'number')]
+        data_table = gviz_api.DataTable(desc)
+        data_table.LoadData(data)
 
-            desc = [('time', 'datetime'), ('key', 'number')]
-            data_table = gviz_api.DataTable(desc)
-            data_table.LoadData(data)
+        del lh
 
-            del lh
-
-            response = data_table.ToJSonResponse(
-                            columns_order=('time', 'key'), order_by='time',
-                            req_id = reqId)
-
-        except Exception as e:
-            print "Something bad happened"
-            request.setResponseCode(500)
-            response = "500: internal server error"
+        response = data_table.ToJSonResponse(
+                        columns_order=('time', 'key'), order_by='time',
+                        req_id = reqId)
 
         return response
 
 
-
     def render_GET(self, request):
 
-        print "Received request:", request.uri
-        print request.path
-        print "args", request.args
-
+        start = time.time()
+        resp = ""
+        respCode = 200
         if request.path == "/":
             # serve the main HTML file
             fh = open("index.html", "r")
-            return fh.read()
+            resp = fh.read()
 
         elif request.path == "/datasource":
-            return self.__handleQuery(request)
+            # for querying GDP
+            try:
+                resp = self.__handleQuery(request)
+            except Exception as e:
+                request.setResponseCode(500)
+                respCode = 500
+                resp = "500: internal server error"
 
         else:
-            return NoResource()
+            # default to 404 not found
+            request.setResponseCode(404)
+            respCode = 404
+            resp = "404: not found"
 
+        end = time.time()
+        print "%s]] GET: <<%s>>\t%d\t%d\t%f" % (time.strftime("%x %X"),
+                                request.uri, respCode, len(resp), end-start) 
+        return resp
 
 
 
@@ -127,5 +130,7 @@ if __name__ == '__main__':
     reactor.listenTCP(args.port, site)
 
     print "Starting web-server on port", args.port
+    print "Log format is: (date)]] (method): <<(requestPath))>> "\
+                "(responseCode) (responseLength) (timeTaken)"
     reactor.run()
 
